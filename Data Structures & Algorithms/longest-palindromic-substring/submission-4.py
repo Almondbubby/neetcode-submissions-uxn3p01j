8ef1f9a892a1dc46dfa3d8e32d9abe884dd7af91
Raymond Fu <79113528+Almondbubby@users.noreply.github.com>
2026-03-30 16:52:45 -0400

    Add: longest-palindromic-substring - submission-4

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-4.py
@@ -0,0 +1,31 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        '''
+        manacher
+        1. preprocess
+        2. loop through
+            3. if index is within bounds of current palindrome, set p[i] to min of mirrored and rest of bound
+            4. otherwise p[i] = 0
+            5. increment p[i] based on if palindromic beyond bounds of current palindrom
+            6. update bounds as necessary
+        7. return the max of p
+        '''
+
+
+        t = '#' + '#'.join(s) + '#'
+        p = [0] * len(t)
+        l = r = 0
+        for i in range(len(t)):
+            if i < r:
+                p[i] = min(r-i, p[l + (r-i)])
+            else:
+                p[i] = 0
+            while (i - p[i] - 1 >= 0 and i + p[i] + 1 < len(t) and t[i+p[i] + 1] == t[i - p[i] - 1]):
+                p[i] += 1
+            if i + p[i] > r:
+                r = i + p[i]
+                l = i - p[i]
+
+        resLen, center_idx = max((v,i) for i, v in enumerate(p))
+        resIdx = (center_idx - resLen) // 2
+        return s[resIdx : resIdx + resLen]
