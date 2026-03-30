c45ca02685135b210359cf31bf87a10df5a4c798
Raymond Fu <79113528+Almondbubby@users.noreply.github.com>
2026-03-30 02:35:24 -0400

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        '''
+        dp
+        base cases:
+        i = 0: 0
+        i = 1: nums[1]
+        recurrence: dp[i] = max(dp[i-1], dp[i-2] + nums[i-2])
+        '''
+        dp = [0 for n in range(len(nums) + 1)]
+        for i in range(len(nums) + 1):
+            if i == 0:
+                dp[i] = 0
+            elif i == 1:
+                dp[i] = nums[0]
+            else:
+                dp[i] = max(dp[i-1], dp[i-2] + nums[i-1])
+        return dp[-1]
